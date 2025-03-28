2930c5f37d8d8ccc2f0a3db5916398741084b6fd
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2025-03-28 13:29:58 +0200

    add buttons for login and toggle for menu, no functionality on them

--- restaurantApp.js
+++ restaurantApp.js
@@ -1,4 +1,7 @@
 import {fetchData} from './fetchData.js';
+import {distance} from './euclidean.js';
+
+let menuType = 'daily';
 //get restaurant data
 const restaurants = await fetchData(
   'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'
@@ -8,16 +11,36 @@ if (restaurants == -2) {
 }
 
 //sort data
-restaurants.sort((a, b) => {
+/*restaurants.sort((a, b) => {
   a.name.toLowerCase();
   b.name.toLowerCase();
   return a.name < b.name ? -1 : 1;
-  /*if (a.name < b.name) {
+  if (a.name < b.name) {
     return -1;
   } else {
     return 1;
-  }*/
-});
+  }
+});*/
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+function success(pos) {
+  const crd = pos.coords;
+  console.log(crd);
+  const startPoint = [crd.longitude, crd.latitude];
+  restaurants.sort((a, b) => {
+    distance(startPoint, a.location.coordinates) -
+      distance(startPoint, b.location.coordinates);
+  });
+  console.log(restaurants);
+}
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
 
 //populate it into html and add modal functionality
 const table = document.querySelector('table');
@@ -45,12 +68,10 @@ for (const restaurant of restaurants) {
     companyP.innerText = restaurant.company;
     const div1 = document.createElement('div');
     div1.append(nameH3, addressP, postalCodeP, cityP, phoneNumberP, companyP);
-    modal.append(div1);
 
     const menu = await fetchData(
       `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${restaurant._id}/fi`
     );
-    console.log(menu);
     if (menu == -2) {
       const errorP = document.createElement('p');
       errorP.innerText = 'Error! Could not retrieve menu!';
@@ -75,8 +96,9 @@ for (const restaurant of restaurants) {
         dialogTable.append(dialogTr);
       }
       div2.append(dialogTable);
-      modal.append(document.createElement('hr'), div2);
+      modal.append(div2);
     }
+    modal.append(document.createElement('hr'), div1);
     modal.showModal();
   });
 
@@ -89,3 +111,18 @@ for (const restaurant of restaurants) {
   tr.append(nameTd, addressTd, cityTd);
   table.append(tr);
 }
+
+//menu type selection
+const dayButton = document.querySelector('#dayMenu');
+const weekButton = document.querySelector('#weekMenu');
+dayButton.addEventListener('click', () => {
+  weekButton.classList.remove('active');
+  dayButton.classList.add('active');
+  menuType = 'daily';
+});
+
+weekButton.addEventListener('click', () => {
+  dayButton.classList.remove('active');
+  weekButton.classList.add('active');
+  menuType = 'weekly';
+});
